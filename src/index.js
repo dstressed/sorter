27c73c6047d4b8e6c975fc89ae253a48c3008188
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-02-21 16:48:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this._arr = [];
+        this._func = function (a, b) {
+            return a - b;
+        };
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this._arr.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this._arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this._arr.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this._arr;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort(function (a, b) {
+            return a - b;
+        });
+        var arr = [];
+        for (var i = 0; i < indices.length; i++) {
+            arr.push(this._arr[indices[i]]);
+        }
+        arr.sort(this._func);
+        for (i = 0; i < arr.length; i++) {
+            this._arr[indices[i]] = arr[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        if (compareFunction) this._func = compareFunction;
+    }
 }
 
 module.exports = Sorter;
